5847aecf30c8099e4d3200d14b5268b5e0ce0bd7
github username <github email address>
2019-08-07 13:33:36 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+const dodger = document.getElementById("dodger") // this selects the element with the Id dodger in this case a div
+
+dodger.style.backgroundColor = "#FF69B4"
+dodger.style.bottom = "0px"
+
+
+
+
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace("px", "")
+    const left = parseInt(leftNumbers, 10)    
+    if (left > 0) { //if left > 0 pixels move if not no
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "")
+    const left = parseInt(leftNumbers, 10)
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+});
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
